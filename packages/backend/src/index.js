f620eabd99942aa8d3b298448a1ebb199607f5ee
Andrei Radu <70237539+aandreiradu@users.noreply.github.com>
2023-01-27 00:07:14 +0200

    backend init (#2)
    Co-authored-by: Andrei Radu <andreiradu@Andreis-MacBook-Pro`>

---
+++ packages/backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res, next) => {
+  return res.status(200).json({
+    message: 'OK!',
+  });
+});
+
+app.listen(5050, () => {
+  console.log('server listening on port 5050');
+});
