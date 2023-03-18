67c591f84643f977704dc00ed5de177db99f676e
Andrei Radu <70237539+aandreiradu@users.noreply.github.com>
2023-03-19 01:10:35 +0200

    Session controllers and services (#11)

--- packages/backend/src/services/transactions.ts
+++ packages/backend/src/services/transactions.ts
@@ -1,6 +1,6 @@
 import prisma from '../utils/prisma';
 import { z } from 'zod';
-import { AuthService } from './auth';
+import { AuthService } from './auth/auth';
 
 const TransactionTypeEnum = z.enum(['Expense', 'Income']);
 const CurrencyEnums = z.enum(['EUR', 'RON']);
@@ -15,8 +15,7 @@ export const createTransactionSchema = z
   })
   .refine(
     (data) => (
-      (data.transactionType as string) != 'Expense' &&
-        (data.transactionType as string) != 'Income',
+      (data.transactionType as string) != 'Expense' && (data.transactionType as string) != 'Income',
       {
         path: ['transactionType'],
         message: 'Invalid transaction type',
@@ -27,35 +26,29 @@ export const createTransactionSchema = z
 export type CreateTransactionArgs = z.infer<typeof createTransactionSchema>;
 
 interface ITransaction {
-  createTransaction(
-    args: CreateTransactionArgs & { userId: string },
-  ): Promise<string>;
+  createTransaction?(args: CreateTransactionArgs & { userId: string }): Promise<string>;
 }
 
 export const TransactionService: ITransaction = {
-  async createTransaction(args: CreateTransactionArgs & { userId: string }) {
-    const { userId: userIdArgs, amount, merchant, transactionType } = args;
-
-    const userId = await AuthService.getUserById(userIdArgs);
-
-    if (!userId) {
-      throw new Error('Could not identify the user by userId');
-    }
-
-    // Create transaction
-    const { id: transactionId } = await prisma.transactions.create({
-      data: {
-        amount: amount,
-        merchant: merchant,
-        type: transactionType,
-        accountId: userId,
-      },
-      select: {
-        id: true,
-      },
-    });
-    console.log('created this transaction', transactionId);
-
-    return transactionId;
-  },
+  // async createTransaction(args: CreateTransactionArgs & { userId: string }) {
+  //   const { userId: userIdArgs, amount, merchant, transactionType } = args;
+  //   const userId = await AuthService.getUserById(userIdArgs);
+  //   if (!userId) {
+  //     throw new Error('Could not identify the user by userId');
+  //   }
+  //   // Create transaction
+  //   const { id: transactionId } = await prisma.transactions.create({
+  //     data: {
+  //       amount: amount,
+  //       merchant: merchant,
+  //       type: transactionType,
+  //       accountId: userId,
+  //     },
+  //     select: {
+  //       id: true,
+  //     },
+  //   });
+  //   console.log('created this transaction', transactionId);
+  //   return transactionId;
+  // },
 };
