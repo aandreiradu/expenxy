9c42ccff51dcc9ec18b6c618d1765eaf05cc0646
Andrei Radu <raduandrei697@gmail.com>
2023-03-24 00:49:13 +0200

    sidebar

--- packages/app/src/components/Sidebar/index.tsx
+++ packages/app/src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { GoogleChromeLogo, Wallet, TrashSimple, CalendarBlank, User, Gear, SignOut, Plus, Bank } from 'phosphor-react';
+import { GoogleChromeLogo, Wallet, TrashSimple, CalendarBlank, User, Gear, SignOut, Plus } from 'phosphor-react';
 import SidebarLink from './SidebarLink';
 import type { SidebarLinkProps } from './SidebarLink';
 import { useAppDispatch } from '../../store/hooks';
@@ -16,14 +16,6 @@ export const sidebarNavigation: SidebarLinkProps[] = [
     href: '/add-transaction',
     icon: <Plus className={sidebarIconClasses} />,
     name: 'AddTransaction',
-    setShowComponent: () => {},
-    isLink: false,
-  },
-  {
-    isLink: true,
-    href: '/create-bank-account?existingAccount=true',
-    icon: <Bank className={sidebarIconClasses} />,
-    name: 'Create Bank Account',
     setShowComponent: () => {},
   },
   {
@@ -106,8 +98,17 @@ const Sidebar = ({ setShowComponent }: ISidebarProps) => {
   }, [error]);
 
   return (
-    <nav className="fixed top-0 left-0 bg-white w-[220px] h-screen flex items-center flex-col ">
+    <nav className="fixed top-0 left-0 bg-white w-[175px] h-screen  items-center flex-col hidden md:flex">
       <h1 className="text-center mt-[20px] mb-[40px] uppercase text-[30px]">expenxy</h1>
+
+      {error && showModal && (
+        <Modal
+          onConfirm={() => setShowModal({ message: '', show: false })}
+          show={showModal.show}
+          title={'Ooops'}
+          message={error.message}
+        />
+      )}
 
       {/* Squares */}
       <div className="shadow-2xl w-[90px] h-[90px] text-center mb-[20px] rounded-[12px] bg-[#1f1f1f] text-white grid content-center justify-center place-items-center  grid-cols-2_20px gap-[10px] twoCols">
@@ -116,20 +117,13 @@ const Sidebar = ({ setShowComponent }: ISidebarProps) => {
         <div className="w-4 h-4 bg-white rounded-sm"></div>
         <div className="w-4 h-4 bg-white rounded-sm"></div>
       </div>
-      <div className="flex flex-col justify-between items-center h-full pb-2">
-        <div className="flex flex-col items-center content-center gap-[20px] md:gap-[34px] mt-5 max-h-[550px] overflow-y-auto px-2">
+      <div className="flex flex-col justify-between items-center h-full pb-10">
+        <div className="flex flex-col items-center content-center gap-[40px] mt-5">
           {sidebarNavigation.map((si) => (
-            <SidebarLink
-              key={si.href}
-              href={si.href}
-              icon={si.icon}
-              setShowComponent={setShowComponent}
-              name={si.name}
-              isLink={si.isLink}
-            />
+            <SidebarLink key={si.href} href={si.href} icon={si.icon} setShowComponent={setShowComponent} name={si.name} />
           ))}
         </div>
-        <div className="p-3 group hover:bg-[#1f1f1f] rounded-md relative before:absolute before:left-0 before:-top-4 before:w-full before:h-[1px] before:bg-gray-400">
+        <div className="p-3 group hover:bg-[#1f1f1f] rounded-md">
           <SignOut
             className=" w-8 h-8 grid te place-self-center my-0 mx-auto rotate-180 group-hover:text-white group-hover:bg-[#1f1f1f]"
             onClick={handleLogOut}
