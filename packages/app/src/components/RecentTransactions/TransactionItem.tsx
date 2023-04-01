e6b3e03cba8bb4a4e7b4fcc085be4c8cdbb53c82
Andrei Radu <raduandrei697@gmail.com>
2023-04-01 21:08:29 +0300

    Recent Transactions

--- packages/app/src/components/RecentTransactions/TransactionItem.tsx
+++ packages/app/src/components/RecentTransactions/TransactionItem.tsx
@@ -8,27 +8,17 @@ interface TransactionItemProps {
   merchantLogoUrl?: ReactNode;
 }
 
-const TransactionItem = ({
-  amount,
-  currency,
-  date,
-  merchant,
-  merchantLogoUrl,
-}: TransactionItemProps) => {
+const TransactionItem = ({ amount, currency, date, merchant, merchantLogoUrl }: TransactionItemProps) => {
   return (
     <div className="flex bg-white w-full items-center rounded-md">
-      <div className="flex-shrink-0 bg-yellow-200 rounded-md">
-        {merchantLogoUrl}
-      </div>
+      <div className="flex-shrink-0 bg-yellow-200 rounded-md">{merchantLogoUrl}</div>
       <div className="mx-4 flex-1 my-auto bg-white">
         <p className="font-medium">{merchant}</p>
         <p className="text-xs font-extralight">{date}</p>
       </div>
       <div className="ml-auto mr-4 flex items-center">
-        <span className="text-xs mt-auto pb-[2px] font-semibold">
-          {currency}
-        </span>
-        <p className="text-lg font-bold">{amount.toFixed(2)}</p>
+        <span className="text-xs mt-auto pb-[2px] font-semibold">{currency}</span>
+        <p className="text-lg font-bold">{amount}</p>
       </div>
     </div>
   );
--- packages/app/src/components/RecentTransactions/index.tsx
+++ packages/app/src/components/RecentTransactions/index.tsx
@@ -1,28 +1,78 @@
 import { AirplaneTilt } from 'phosphor-react';
 import TransactionItem from './TransactionItem';
-
+import { selectLatestTransactions } from '../../store/User/index.selector';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHttpRequest } from '../../hooks/useHttp';
+import { useEffect } from 'react';
+import statsAndMaps from '../../config/statusAndMessagesMap';
+import { setLatestTransactions } from '../../store/User/index.slice';
+import Loader from '../Loader';
+import { PropagateLoader } from 'react-spinners';
 const RecentTransactions = () => {
+  const dispatch = useDispatch();
+  const { sendRequest, isLoading, error } = useHttpRequest();
+  const { latestTransactions } = useSelector(selectLatestTransactions);
+
+  useEffect(() => {
+    const fetchLatestTransactions = async () => {
+      if (latestTransactions.length === 0) {
+        const response = await sendRequest({
+          method: 'GET',
+          withCredentials: true,
+          url: '/transactions/latest',
+        });
+
+        console.log('response', response);
+        if (response) {
+          const { data, status, message } = response;
+
+          if (status === 200 && message === statsAndMaps['fetchLatestTransactionsSuccessfully']?.message) {
+            console.log('all good, can dispatch');
+            const { latestTransactions } = data;
+            console.log('latestTransactions', latestTransactions);
+            dispatch(setLatestTransactions({ latestTransactions }));
+          }
+        }
+      } else {
+        console.log('transactions already in store, no need to fetch again');
+      }
+    };
+    fetchLatestTransactions();
+  }, []);
+
   return (
-    // <div className="w-full max-w-md mt-auto h-[350px] max-h-[400px] ">
-    <div className="w-full h-[350px] max-h-[400px] ">
-      <div className="w-full flex justify-between items-center py-3">
+    <div className="relative w-full h-full max-h-[350px] overflow-hidden">
+      <div className="sticky top-0 left-0 w-full flex justify-between items-center py-3">
         <p className="text-black font-bold text-lg">Recent Transactions</p>
-        <span className="cursor-pointer relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-gray-400 px-5 leading-7 text-[12px] text-gray-400">
-          View All
-        </span>
+        {latestTransactions?.length > 0 && (
+          <span className="cursor-pointer relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:bg-gray-400 px-5 leading-7 text-[12px] text-gray-400">
+            View All
+          </span>
+        )}
       </div>
-      <div className="w-full flex flex-col items-center justify-center gap-4">
-        {[...Array(4).keys()].map((_transaction) => (
-          // eslint-disable-next-line react/jsx-key
-          <TransactionItem
-            key={_transaction}
-            amount={25}
-            currency={'$'}
-            date={'2023-01-26'}
-            merchant={'eMAG'}
-            merchantLogoUrl={<AirplaneTilt className="h-16 w-16 p-4" />}
-          />
-        ))}
+      <div className="relative w-full h-full max-h-[300px] flex flex-col items-center gap-3 overflow-y-auto">
+        {isLoading ? (
+          <div className="max-w-md mt-5">
+            <Loader
+              icon={<PropagateLoader color="#1f1f1f" className="p-2" />}
+              loadingText="Getting your latest transactions"
+              textClasses="text-[#1f1f1f] text-lg  uppercase"
+            />
+          </div>
+        ) : latestTransactions?.length > 0 ? (
+          latestTransactions?.map((tr) => (
+            <TransactionItem
+              key={tr.transactionId}
+              amount={Number(tr.amount)}
+              currency={tr.currencySymbol || tr.currency}
+              date={new Date(tr.date).toLocaleDateString('ro-RO')}
+              merchant={tr.merchant || 'Unknown'}
+              merchantLogoUrl={<AirplaneTilt className="h-16 w-16 p-4" />}
+            />
+          ))
+        ) : (
+          <p className="mt-3 text-base capitalize font-bold">No transactions recorded</p>
+        )}
       </div>
     </div>
   );
