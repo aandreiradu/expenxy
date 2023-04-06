786e5a723ebbeba4c5febc33b5f946e1f4c32cf9
Andrei Radu <raduandrei697@gmail.com>
2023-04-07 00:57:02 +0300

    Added status,name,open at,expires at to widget

--- packages/app/src/components/CardInfoWidget/cardInfoWidget.tsx
+++ packages/app/src/components/CardInfoWidget/cardInfoWidget.tsx
@@ -9,21 +9,27 @@ export interface ICartInfoWidget {
 const CardInfoWidget: FC<ICartInfoWidget> = ({ selectedAccount }) => {
   const accountData = useSelector(selectAccountById(selectedAccount));
 
+  console.log('accountData', accountData);
+
   return (
-    <div className="w-full max-w-[250px] h-full flex items-center flex-col flex-1 bg-white">
-      <h3 className="text-center text-gray-500 h-1/5 flex items-end mb-3">Card Info</h3>
-      <div className="w-full bg-red-500 grid grid-cols-2 p-3 overflow-hidden">
-        <div className="w-full flex flex-col justify-end text-left">
-          <p>Status</p>
-          <p>Balance</p>
-          <p>Opened at</p>
+    <div className="w-full max-w-[275px] h-full flex items-center flex-col p-2 flex-1 bg-white rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+      <h3 className="text-center text-gray-500 flex items-end mb-3">Card Info</h3>
+      <div className="w-full h-full grid grid-cols-2 p-3 overflow-hidden gap-x-2">
+        <div className="w-full flex h-full flex-col justify-start text-left text-base gap-2">
+          <span className="text-base">Status</span>
+          <span className="text-base">Name</span>
+          <span className="text-base">Balance</span>
+          <span className="text-base">Opened at</span>
+          <span className="text-base">Expires at</span>
         </div>
-        <div className="w-full flex flex-col justify-start text-right">
-          <p>Active</p>
-          <p>
+        <div className="w-full h-full flex flex-col justify-start text-right gap-2">
+          <span className="text-base font-bold">{accountData?.status}</span>
+          <span className="text-base font-bold">{accountData?.name}</span>
+          <span className="text-base font-bold">
             {accountData?.balance} {accountData?.currency.code}
-          </p>
-          <p>{new Date().toLocaleDateString()}</p>
+          </span>
+          <span className="text-base font-bold">{new Date(accountData?.createdAt || '').toLocaleDateString()}</span>
+          <span className="text-base font-bold">{new Date(accountData?.expiresAt || '').toLocaleDateString()}</span>
         </div>
       </div>
     </div>
